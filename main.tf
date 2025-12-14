38e50ff6161a0733742a2d2888007626d551d8dc
Noah Van Laer <noahvanlaer@gmail.com>
2025-12-14 10:12:05 +0100

    Configure S3 remote backend

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "crypto-pipeline-tfstate-nvan-lae"
+    key    = "terraform.tfstate"
+    region = "eu-west-3"
+  }
+}
+
 provider "aws" {
   region = "eu-west-3"
 }
