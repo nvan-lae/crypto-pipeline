073c90284c7b4bbb2fcca8ab07fbc9bac4d50b95
Noah Van Laer <noahvanlaer@gmail.com>
2025-12-19 13:48:30 +0100

    Edit TimeOut ReaderLambda & Change Graph

--- main.tf
+++ main.tf
@@ -263,6 +263,7 @@ resource "aws_lambda_function" "reader_lambda" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "reader.lambda_handler"
   runtime       = "python3.9"
+  timeout       = 10
 
   environment {
     variables = {
